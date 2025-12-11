0c9f5450026409e026c3b7c18409147ee9235f5d
xavmer <xavier@lefflers.com>
2025-12-10 19:05:20 -0600

    should work on pages now

--- mt-olympus-visualization/next.config.ts
+++ mt-olympus-visualization/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Mt-Olympus-Visualization",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
